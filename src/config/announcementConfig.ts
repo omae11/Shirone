fd4d9d038a25e6679fcd7a093b0feeaa1559893f
omae11 <omae11@users.noreply.github.com>
2026-09-05 14:59:07 +0800

    feat(config): update announcement

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -9,12 +9,12 @@ export const announcementConfig: AnnouncementConfig = withUserConfig(
 	"announcement",
 	{
 		title: "", // 公告标题，填空使用 i18n 字符串 Key.announcement
-		content: "The only way to do great work is to love what you do", // 公告内容
+		content: "亖月三日君寻的学习记录blog", // 公告内容
 		closable: true, // 允许用户关闭公告
 		link: {
 			enable: true, // 启用链接
 			text: "GitHub", // 链接文本
-			url: "https://github.com", // 链接 URL
+			url: "https://github.com/omae11", // 链接 URL
 			external: true, // 外部链接
 		},
 	},
